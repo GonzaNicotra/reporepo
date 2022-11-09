78fe52585b83ab849a1b7dbd191c420190efff2f
Gonzalo Nicotra <gonzalonicotra@hotmail.com>
2022-11-08 21:25:16 -0300

    Third commit

--- src/Pages/Inicio/Inicio.jsx
+++ src/Pages/Inicio/Inicio.jsx
@@ -1,40 +1,34 @@
 import React from 'react';
 import './Inicio.css';
 import MyBackgroundImage from '../../assets/inicio.webp'
-import LogoBunima from '../../assets/logoBunima.jpeg'
+import LogoBunima from '../../assets/logoBunima.webp'
 import LD from '../../assets/diseñologo.png'
 import MG from '../../assets/megafono.png'
 import DW from '../../assets/diseñoweb.png'
 import IF from '../../assets/inicioFooter.jpeg'
 import Ofi from '../../assets/ofi.jpg'
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import Contacto from '../Contacto/Contacto';
 
 
 export default function Inicio() {
 
   return (
     <>
-      <div className='inicio' style={{
-        backgroundImage: `url(${MyBackgroundImage})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: '500px',
-        width: 'auto'
-    }} >
-      <div className='inicioDiv'>
-        <img src={LogoBunima} alt="" className='logoInicio d-inline-flex' />
-        <h5>Ayudamos a crecer a tu marca</h5>
-        <h1 className='inicioTitulo'>Full Service Digital Marketing</h1>
-        <h4>Nos encargamos en crecer tu negocio con múltiples servicios de marketing digital</h4>
-        <div className='row m-auto'>
+      <div className='inicioFinal'>
+        <div className='inicioFinalImg'>
+          <img className=' imagenOfi' src={Ofi} alt="" />
+        </div>
+        <div className='inicioFinalDiv'>
+          <h1 style={{ color: 'white', fontWeight:'bold'}}>Burla a la competencia con nuestras campañas</h1>
+          <h5 style={{ color: 'white' }}>Obtén mas trafico. Adquirí mas clientes. Vende mas productos. Bunima trabaja con todo tipo de negocios</h5>
           <Link to={'/contacto'}>
-          <button className='col bComprar m-1'>Comunicate</button>
+            <Button className='bComprar'>Contactanos</Button>
           </Link>
-          <a href="#Servicios"><button className='col bComprar m-1'>Nuestros Servicios</button></a>
         </div>
       </div>
-      </div>
-      {/*  */}
+      <Contacto/>
       <div className='inicioPreg'>
         <p className='inicioSub'>Listo para crecer con una agencia digital de marketing? Consigue tu propuesta gratis hoy:</p>
         <h3 style={{color: 'grey'}}>Nuestra agencia digital se encargo personalmente de obtener las mejores campañas de marketing digital utilizadas por los mejores empresas, negocios y emprendimientos. Pedí hoy tu estrategia gratis y se parte de la nueva generación.</h3>
@@ -72,22 +66,10 @@ export default function Inicio() {
         <h1 className='imagenDos'>Nos preocupamos por cada cliente</h1>
         <p style={{ color: '#ffffff'}}>Creamos un ambiente seguro en la que el cliente sienta que cualquier cosa que necesite pueda pedirlo,  nosotros nos ocupamos y brindar el mejor servicio posible</p>
         <Link to={'/contacto'}>
-          <button className='bComprar'> Contactanos</button>
+          <Button className='bComprar'> Contactanos</Button>
         </Link>
       </div>
-      <div className='inicioFinal'>
-        <div className='inicioFinalImg'>
-          <img className=' imagenOfi' src={Ofi} alt="" />
-        </div>
-        <div className='inicioFinalDiv'>
-          <h5 style={{ color: 'green', fontWeight:'bold'}}>Creamos Soluciones</h5>
-          <h2 style={{ color: 'rgb(7, 13, 89)', fontWeight:'bold'}}>Todo nuestro equipo se concentra en un cliente</h2>
-          <h5>Brindamos toda la atención para que el objetivo se cumpla, y se consiga el pedido</h5>
-          <Link to={'/contacto'}>
-            <button className='bComprar'>Contactanos</button>
-          </Link>
-        </div>
-      </div>
+      
     </>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import LogoBuNiMa from '../../assets/logoBunima.jpeg';
+import LogoBuNiMa from '../../assets/logoBunima.webp';
 import './Navbar.css';
 import { NavLink } from 'react-router-dom';
 
@@ -18,6 +18,12 @@ function BasicNav() {
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className='ms-auto'>
+            <NavLink to={'/contacto'} className='detail'>
+              <Nav.Link active href="#link">About</Nav.Link>
+            </NavLink>
+            <NavLink to={'/contacto'} className='detail'>
+              <Nav.Link active href="#link">Book</Nav.Link>
+            </NavLink>
             <NavLink to={'/contacto'} className='detail'>
               <Nav.Link active href="#link">Contacto</Nav.Link>
             </NavLink>
